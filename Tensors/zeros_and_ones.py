dcd4b7420666c049db41135bba02e1e693833bc9
Amedeo Bozzoli <amedeobozzoli2003@gmail.com>
2024-07-24 15:48:50 +0200

    zeros and ones

---
+++ Tensors/zeros_and_ones.py
@@ -0,0 +1,9 @@
+import torch
+
+# Create a tensor of all zeros
+tensor_zeros = torch.zeros((2, 3, 3))
+print(tensor_zeros)
+
+# Create a tensor of all ones
+tensor_ones = torch.ones((2, 3, 3))
+print(tensor_ones)
